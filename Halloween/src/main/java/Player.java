ae7871d7d4384dc4cace7cc2553fac35868db56d
Ella <ellaelonen@gmail.com>
2018-10-15 16:39:13 +0200

    Cleanup

--- Halloween/src/main/java/Player.java
+++ Halloween/src/main/java/Player.java
@@ -1,34 +1,3 @@
 public class Player {
 
-    int amountOfLivesLeft;
-    double x;
-    double y;
-    int speed;
-
-    public Player(int amountOfLives, int XPosition, int YPosition, int speed){
-    	
-        amountOfLivesLeft = this.amountOfLivesLeft;
-        x = this.x;
-        speed = this.speed;
-    }
-
-    public void SetLives() {
-        amountOfLivesLeft = this.amountOfLivesLeft;
-    }
-
-    public void setTestSpeed() {
-        this.speed = speed;
-    }
-
-    public void JumpToPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public void move(){
-
-    }
-
-    public void equals(){
-    }
 }
