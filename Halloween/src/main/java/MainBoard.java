c411371d6f9b28ae00d6680a3c9a4365ebb41863
Ella <ellaelonen@gmail.com>
2018-10-15 10:21:19 +0200

    Classes, Player, Player test

--- Halloween/src/main/java/MainBoard.java
+++ Halloween/src/main/java/MainBoard.java
@@ -1,6 +1,8 @@
 
 public class MainBoard {
 	
+	int timer;
+	
     public static void main(String[] args) {
         System.out.print("Main");
         MainBoard game = new MainBoard();
--- Halloween/src/main/java/Player.java
+++ Halloween/src/main/java/Player.java
@@ -1,19 +1,34 @@
 public class Player {
 
-	String totalAmountOfLives;
-    String amountOfLivesLeft;
-    boolean alive;
-    String state;
-    int position;
+    int amountOfLivesLeft;
+    double x;
+    double y;
     int speed;
 
-    public Player(){
+    public Player(int amountOfLives, int XPosition, int YPosition, int speed){
     	
-    	totalAmountOfLives = this.totalAmountOfLives;
         amountOfLivesLeft = this.amountOfLivesLeft;
-        alive = this.alive;
-        state = this.state;
-        position = this.position;
+        x = this.x;
         speed = this.speed;
+    }
+
+    public void SetLives() {
+        amountOfLivesLeft = this.amountOfLivesLeft;
+    }
+
+    public void setTestSpeed() {
+        this.speed = speed;
+    }
+
+    public void JumpToPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(){
+
+    }
+
+    public void equals(){
     }
 }
