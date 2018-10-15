ab515339c0a8dc33fd11482562e6bd44e63178cc
Katya <ekaterinaqvist@gmail.com>
2018-10-15 16:52:20 +0200

    Testar att puscha en kommentar

--- Halloween/src/main/java/MainApp.java
+++ Halloween/src/main/java/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.print("Main");
         MainApp app = new MainApp();
+        //ville bara testa att puscha
     }
 }
