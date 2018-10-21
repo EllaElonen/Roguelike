a1975a8c6a2ca5047bdcd64c2fae2c11a6c71fd0
Katya <ekaterinaqvist@gmail.com>
2018-10-21 17:29:18 +0200

    Tester

--- src/main/java/roguelike/Item.java
+++ src/main/java/roguelike/Item.java
@@ -1,5 +1,17 @@
 package roguelike;
 
 public class Item {
+	String name;
 
+	public Item(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+	public String toString() {
+		return name;
+	}
 }
--- src/main/java/roguelike/Player.java
+++ src/main/java/roguelike/Player.java
@@ -1,8 +1,11 @@
 package roguelike;
 
 import java.awt.Point;
+import java.util.HashSet;
 
 public class Player extends Actor {
+
+	private HashSet<Item> items = new HashSet<>();
 
 	public Player(Point position, String name, int healthPoints, int speed, int intelligence) {
 		super(position, name, healthPoints, speed, intelligence);
@@ -13,4 +16,40 @@ public class Player extends Actor {
 		double newY = getPosition().getY() + 1.0;
 		Point newPoint = new Point((int) newX, (int) newY);
 	}
+
+	public boolean addItemToInventory(Item item) {
+		if (items.size() < 30) {
+			items.add(item);
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public boolean itemExists(Item item) {
+		return items.contains(item);
+	}
+
+	public int getInventorySize() {
+		return items.size();
+
+	}
+
+	// ger referenser till alla Items
+	public HashSet<Item> getItems() {
+		return items;
+
+	}
+
+	public boolean dropItemFromInventory(Item item) {
+		if (items.contains(item)) {
+			items.remove(item);
+			return true;
+		}
+
+		return false;
+
+	}
+
 }
